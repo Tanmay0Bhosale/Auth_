688e01db8eda7fd1bf1b81d0049d9f921ad6e4be
Tanmay <tanmaytanvi1234@gmail.com>
2025-03-19 21:42:16 +0530

    bcrypt learned (genSalts)

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const app = express();
 
-app.use(cookieParser());
+// app.use(cookieParser());
 
 app.get('/', function(req,res){
-    res.cookie("name","tanmay");
-    res.send("Done");
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash("password", salt, function(err, hash) {
+           console.log(hash);
+        });
+    });
 });
 
-app.get('/read', function(req,res){
-    console.log(req.cookies);
-    res.send("read page");
-});
+
 app.listen(3000, function(){
     console.log('server is running on port 3000');
 });
