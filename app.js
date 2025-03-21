8fc09d44e846028d867e08828420af864c721c85
Tanmay <tanmaytanvi1234@gmail.com>
2025-03-22 02:43:25 +0530

    done

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 const express = require('express');
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const app = express();
 
-// app.use(cookieParser());
+app.use(cookieParser());
 
 app.get('/', function(req,res){
-    // bcrypt.genSalt(10, function(err, salt) {
-    //     bcrypt.hash("password", salt, function(err, hash) {
-    //        console.log(hash);
-    //     });
-    // });
-
-    bcrypt.compare("password", "$2b$10$DtzAhe8ROfdnlrIpGZBQwOXqPkls7BAFumz4GGY.cLwRanI5iwEay", function(err, result) {
-        console.log(result);
-    });
+    let token = jwt.sign({user: "tanmaytanvi1234@gmail.com"},"secret");
+    res.cookie('token', token);
+    res.send('Cookie is set');
 });
 
+app.get('/read' , function(req,res) {
+    let data = jwt.verify(req.cookies.token, "secret");
+    console.log(data);
+});
 
 app.listen(3000, function(){
-    console.log('server is running on port 3000');
+    // console.log('server is running on port 3000');
 });
